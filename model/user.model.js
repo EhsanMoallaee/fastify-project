9fa09ce8029987dc4225b3baf0bd1269a1b5ca37
Ehsan Moallaee <ehsanm78@gmail.com>
2023-08-24 09:55:58 +0330

    User model class created

--- model/user.model.js
+++ model/user.model.js
@@ -1,3 +1,21 @@
+import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/sequelize.config";
 
-// sequelize
+export class User extends Model {}
+
+User.init({
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    first_name: { type: DataTypes.STRING },
+    last_name: { type: DataTypes.STRING },
+    username: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    active: { type: DataTypes.BOOLEAN, defaultValue: false },
+    birthday: { type: DataTypes.DATE },
+}, {
+    sequelize,
+    name: 'users'
+})
+
+User.sync({ force: true }).then(result => {
+    console.log('User Table : ', result);
+})
